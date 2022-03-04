5236bbab3ed9ecee104b8f21e1cdba94c8c2c3c5
Isaac Schemm <isaacschemm@gmail.com>
2022-03-04 15:49:17 -0500

    Combine everything into one file

---
+++ ISchemm.UTF32/Library.fs
@@ -0,0 +1,93 @@
+﻿namespace ISchemm.UTF32
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Runtime.InteropServices
+open System.Text
+open Microsoft.FSharp.NativeInterop
+
+[<Struct>]
+type Char32 = {
+    Value: int32
+} with
+    static member Encoding = new UTF32Encoding(not BitConverter.IsLittleEndian, false)
+
+    override this.ToString () =
+        this.Value
+        |> BitConverter.GetBytes
+        |> Char32.Encoding.GetString
+
+module Char32Array =
+    let FromByteArray (bArr: byte[]): Char32[] =
+        if bArr.Length % sizeof<Char32> <> 0 then
+            raise (FormatException $"Length of array must be divisible by {sizeof<Char32>}")
+
+        if bArr.Length = 0 then
+            [||]
+        else
+            let cArr = Array.zeroCreate<Char32> (bArr.Length / sizeof<Char32>)
+            use cPtr = fixed cArr in Marshal.Copy (bArr, 0, NativePtr.toNativeInt cPtr, bArr.Length)
+            cArr
+
+    let FromString (str: string): Char32[] =
+        str
+        |> Char32.Encoding.GetBytes
+        |> FromByteArray
+
+    let GetByteArray (cArr: Char32[]): byte[] =
+        if cArr.Length = 0 then
+            [||]
+        else
+            let bArr = Array.zeroCreate<byte> (cArr.Length * sizeof<Char32>)
+            use cPtr = fixed cArr in Marshal.Copy (NativePtr.toNativeInt cPtr, bArr, 0, bArr.Length)
+            bArr
+
+    let GetString (array: Char32[]): string =
+        array
+        |> GetByteArray
+        |> Char32.Encoding.GetString
+
+[<Struct>]
+type String32 = {
+    List: Char32 list
+} with
+    static member FromList (list: Char32 list) = { List = list }
+
+    static member FromEnumerable (src: seq<Char32>) =
+        src
+        |> List.ofSeq
+        |> String32.FromList
+
+    static member ToEnumerable (src: String32) =
+        src :> seq<Char32>
+
+    static member FromString (str: string) =
+        str
+        |> Char32Array.FromString
+        |> String32.FromEnumerable
+
+    static member ToString (src: String32) =
+        src.List
+        |> Array.ofList
+        |> Char32Array.GetString
+
+    static member Empty = { List = [] }
+
+    member this.Length = List.length this.List
+    member this.Item index = this.List.[index]
+
+    member this.GetSlice (startIndex, endIndex) =
+        this.List.GetSlice (startIndex, endIndex)
+        |> String32.FromList
+
+    member this.Substring (startIndex) = this.[startIndex..]
+    member this.Substring (startIndex, length) = this.[startIndex..startIndex + length - 1]
+
+    override this.ToString () = String32.ToString this
+
+    interface IReadOnlyList<Char32> with
+        member this.Count: int = this.Length
+        member this.GetEnumerator(): IEnumerator = (this.List :> IEnumerable).GetEnumerator()
+        member this.GetEnumerator(): IEnumerator<Char32> = (this.List :> seq<Char32>).GetEnumerator()
+        member this.Item with get (index: int): Char32 = this.List.[index]
